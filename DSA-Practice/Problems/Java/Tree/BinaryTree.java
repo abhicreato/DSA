4e7a78f069f3672eb49f161de8d9afeb43264688
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2022-09-09 10:21:51 +0530

    directory change

---
+++ DSA-Practice/Problems/Java/Tree/BinaryTree.java
@@ -0,0 +1,22 @@
+public class BinaryTree{
+	
+	public class Node{
+		int data;
+		Node Right;
+		Node Left;
+
+		Node(data){
+			this.data =data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	
+	public static void main(String a[]){
+		
+		int NodeData = {1,};
+		
+		
+		
+	}
+}
