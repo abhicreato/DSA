a158dbb06cd9157965862f02dc79d7f7acb3c90a
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2022-10-25 11:28:41 +0530

    Update KnapSack.java

--- DSA-Practice/Problems/Java/DP/KnapSack.java
+++ DSA-Practice/Problems/Java/DP/KnapSack.java
@@ -33,3 +33,35 @@ class KnapSack {
         
     }
 }
+
+class knapSack2{
+     
+    static int cost[][];
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         
+        cost = new int[n + 1][W + 1];
+        
+        // Iterative soln 
+        for(int i=0;i<cost.length;i++){
+            for(int j =0;j < cost[i].length;j++)
+            if(i == 0 || j == 0) cost[i][j] = 0; 
+            
+        }
+        
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < W + 1; j++){
+                if(j >= wt[i - 1]){
+                    cost[i][j] = Math.max(val[i - 1] + cost[i - 1][j - wt[i - 1]], cost[i - 1][j]);
+                }else{
+                    cost[i][j] = cost[i - 1][j];
+                }
+            }
+        } 
+        
+        return cost[n][W];
+
+    } 
+
+}
