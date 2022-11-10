7cfb11b616f57e16da76fd17cefffc736b74dc33
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2022-11-10 10:59:39 +0530

    Update MatrixChainMultiplication.java

--- DSA-Practice/Problems/Java/DP/MatrixChainMultiplication.java
+++ DSA-Practice/Problems/Java/DP/MatrixChainMultiplication.java
@@ -1,12 +1,15 @@
 package DP;
 
 public class MatrixChainMultiplication {
-
+    
     static int ans;
+    static int [][] dp;
     
     static int matrixMultiplication(int N, int arr[])
     {
         // code here
+        dp = new int [N+1][N+1];
+        
         return solve(arr, 1, N - 1);
     }
     
@@ -14,18 +17,20 @@ public class MatrixChainMultiplication {
         
         if(i >= j) return 0;
         
+        if(dp[i][j] != 0) return dp[i][j];
+        
         int min = Integer.MAX_VALUE;
         
-        for(int k = i ; k <= j-1; k++){
+        for(int k = i ; k <= j - 1; k++){
             
-            int temp = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i-1] * arr[k] * arr[j]);
+            int temp = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
              
             min = Math.min(min, temp); 
             
         }
         
-        return min;
+       return dp[i][j] = min;
         
     }
-    
+
 }
