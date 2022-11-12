457c0c5aa1be82308f1f0e6abea6eb1acb4798de
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2022-11-12 10:40:28 +0530

    Update PalindromicPartitioning.java

--- DSA-Practice/Problems/Java/DP/PalindromicPartitioning.java
+++ DSA-Practice/Problems/Java/DP/PalindromicPartitioning.java
@@ -1,7 +1,6 @@
 package DP;
 
 public class PalindromicPartitioning {
-
     static int dp[][];
     
     static int palindromicPartition(String str)
@@ -18,13 +17,18 @@ public class PalindromicPartitioning {
         
         if(i >= j || isPali(s, i, j)) return 0;
         
+        // Memoaization 
         if(dp[i][j] !=0) return dp[i][j];
         
         int min = Integer.MAX_VALUE;
         
         for(int k = i; k<=j-1; k++){
             
-            int temp = solve(s, i, k) + solve(s, k + 1, j) + 1;
+             // Memoaization more optimised 
+            int left = (dp[i][k] != 0) ? dp[i][k] : solve(s, i, k);
+            int right = (dp[k + 1][j] != 0) ? dp[k + 1][j] : solve(s, k + 1, j);
+
+            int temp = 1 + left + right;
             
             min = Math.min(min, temp);
         }
