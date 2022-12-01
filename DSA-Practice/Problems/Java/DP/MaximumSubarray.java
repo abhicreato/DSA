a5855d49c0a1f3b9386760e895c1b5124f04327a
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2022-12-01 11:17:24 +0530

    Create MaximumSubarray.java

---
+++ DSA-Practice/Problems/Java/DP/MaximumSubarray.java
@@ -0,0 +1,41 @@
+package DP;
+
+public class MaximumSubarray {
+
+    public int maxSubArray(int[] nums) {
+        
+        //return solve(nums, 0,0);
+        
+        // Kadane's Algorithm
+        int max_so_far = 0;
+        int ans = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < nums.length; i++) {
+            max_so_far += nums[i];
+            
+            ans = Math.max(ans, max_so_far);
+            
+            max_so_far = Math.max(max_so_far, 0);
+        }
+        
+        return ans;
+        
+    }
+    
+    public int solve(int[] num, int currIndex, int sum){
+        
+        
+        return 0;
+    }
+    
+}
+
+/*
+**************** Logic ****************
+DP:
+Similar to 0-1 Knapsack.
+1. We must pick the current element if the previous element was selected.
+2. We may or may not pick the current element if the previous element was not selected.
+Kadane's Algorithm:
+If at any point sum becomes negative then no point keeping it because 0 is obviously greater than negative, so just make your sum 0.
+*/
