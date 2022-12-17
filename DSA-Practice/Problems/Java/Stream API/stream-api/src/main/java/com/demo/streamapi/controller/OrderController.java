b60742a893eb0d824fcacd9c69228b640ea7bde1
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2022-12-17 13:09:37 +0530

    updates

---
+++ DSA-Practice/Problems/Java/Stream API/stream-api/src/main/java/com/demo/streamapi/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.demo.streamapi.controller;
+
+import com.demo.streamapi.entity.Order;
+import com.demo.streamapi.repository.OrderRepository;
+import com.demo.streamapi.service.OrderManagement;
+import com.demo.streamapi.service.OrderManagementStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    OrderManagement orderManagement;
+
+    @GetMapping("/")
+    public List<Order> getAllOrders(){
+        return orderRepository.findAll();
+    }
+}
--- DSA-Practice/Problems/Java/Stream API/stream-api/src/main/java/com/demo/streamapi/controller/ProductController.java
+++ DSA-Practice/Problems/Java/Stream API/stream-api/src/main/java/com/demo/streamapi/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.demo.streamapi.controller;
 import com.demo.streamapi.entity.Product;
 import com.demo.streamapi.repository.OrderRepository;
 import com.demo.streamapi.repository.ProductRepository;
+import com.demo.streamapi.service.OrderManagement;
 import com.demo.streamapi.service.OrderManagementStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class ProductController {
     ProductRepository productRepository;
 
     @Autowired
-    OrderManagementStream orderManagementStream;
+    OrderManagement orderManagement;
 
     @GetMapping("/")
     public List<Product> getAllProduct(){
@@ -26,6 +27,6 @@ public class ProductController {
 
     @GetMapping("/{category}")
     public List<Product> getAllProductByCategoryAndPriceGreaterThan(@PathVariable String category, @RequestParam double priceGreaterThan){
-        return orderManagementStream.getProductByCategoryAndPriceGreaterThan(category, priceGreaterThan);
+        return orderManagement.getProductByCategoryAndPriceGreaterThan(category, priceGreaterThan);
     }
 }
