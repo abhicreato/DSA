43e704e066bf8b1a01d9ca19c2e768d444693224
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2025-07-05 00:16:03 +0530

    Refactor subarraySum3 method and improve comments for clarity

--- DSA-Practice/Problems/Java/Array/SubarraySumEqualsK.java
+++ DSA-Practice/Problems/Java/Array/SubarraySumEqualsK.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class SubarraySumEqualsK {
 
+    //map while
     public int subarraySum(int[] nums, int k) {
 
         int j = 0;
@@ -37,6 +38,24 @@ public class SubarraySumEqualsK {
                 if (sum == k)
                     count++;
             }
+        }
+
+        return count;
+    }
+
+    //Map for
+    public static int subarraySum3(int[] nums, int k) {
+
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap();
+        int sum = 0;
+        map.put(0, 1);
+        for (int n : nums) {
+            sum += n;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
         return count;
