0279323a2494547359056bb5a781c932f8d157d2
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2025-07-05 19:25:08 +0530

    Refactor TrappingRainWater class to simplify variable names and improve readability

--- DSA-Practice/Problems/Java/Array/TrappingRainWater.java
+++ DSA-Practice/Problems/Java/Array/TrappingRainWater.java
@@ -1,38 +1,29 @@
 public class TrappingRainWater {
 
     public int trap(int[] height) {
+        int ans = 0;
+        int length = height.length;
+
         
-        int l = height.length;
-        int maxAtRight[] = new int[l];
-        int maxAtLeft[] = new int[l];
-        int ans =0;
-        
-        maxAtLeft[0] = height[0];
-        for(int i=1;i<l;i++){
-            if(maxAtLeft[i-1] > height[i]){
-                maxAtLeft[i] = maxAtLeft[i-1];
-            }else{
-                maxAtLeft[i] = height[i];
-            }
-        }
-        
-        maxAtRight[l-1] = height[l-1];
-        for(int i=l-2;i>=0;i--){
-            if(maxAtRight[i+1] > height[i]){
-                maxAtRight[i] = maxAtRight[i+1];
-            }else{
-                maxAtRight[i] = height[i];
-            }
+        int[] maxInLeft = new int[length];
+        maxInLeft[0] = height[0];
+        for (int i = 1; i < length; i++) {
+            maxInLeft[i] = Math.max(maxInLeft[i - 1],height[i]);
         }
 
-        for(int i=0;i<l;i++){
-            
-            int min = Math.min(maxAtRight[i],maxAtLeft[i]);
-            
-            ans = ans + (min - height[i]);
-            
+        int[] maxInRight = new int[length];
+        maxInRight[length - 1] = height[length - 1];
+        for (int i = length - 2; i >= 0; i--) {
+            maxInRight[i] = Math.max(maxInRight[i + 1],height[i]);
         }
-        
+
+        for (int i = 0; i < length; i++) {
+
+            int min = Math.min(maxInRight[i], maxInLeft[i]);
+
+            ans = ans + (min - height[i]);
+        }
+
         return ans;
         
     }
