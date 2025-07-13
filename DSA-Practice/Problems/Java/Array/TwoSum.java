e06ebf42ef717045b3f6af3bc354de9b95a84e5c
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2025-07-13 18:57:33 +0530

    improve

--- DSA-Practice/Problems/Java/Array/TwoSum.java
+++ DSA-Practice/Problems/Java/Array/TwoSum.java
@@ -4,33 +4,33 @@ import java.util.Map;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        
-        int [] op = new int [2];
-        Map<Integer,Integer> m = new HashMap();
 
-        for(int i=0;i<nums.length;i++){
+        int[] op = new int[2];
+        Map<Integer, Integer> map = new HashMap();
 
-            if(m.containsKey(nums[i])){
-                op[0] = m.get(nums[i]);
-                op[1] = i;
-                return op;
-            }else{
-                m.put(target - nums[i], i);
+        for (int i = 0; i < nums.length; i++) {
+
+            int current = nums[i];
+
+            if (map.containsKey(nums[i])) {
+                return new int[] { map.get(current), i };
             }
+
+            map.put(target - current, i);
+
         }
 
         return op;
 
     }
-    
 
     public int[] twoSum2(int[] nums, int target) {
 
-        int [] op = new int [2];
+        int[] op = new int[2];
 
-        for(int i=0;i<nums.length;i++){
-            for(int j = i + 1;j<nums.length;j++){
-                if(nums[i] + nums[j] == target){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
                     op[0] = i;
                     op[1] = j;
                     return op;
