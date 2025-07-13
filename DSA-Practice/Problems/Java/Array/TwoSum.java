28106adb62fdfb0218af9b4820fb2305469925b3
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2025-07-13 18:54:10 +0530

    brute force two sum

--- DSA-Practice/Problems/Java/Array/TwoSum.java
+++ DSA-Practice/Problems/Java/Array/TwoSum.java
@@ -1,26 +1,43 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
         
-        Map<Integer,Integer> map = new HashMap<>();
-        int[] ans = new int[2];
-        
+        int [] op = new int [2];
+        Map<Integer,Integer> m = new HashMap();
+
         for(int i=0;i<nums.length;i++){
-            
-            if(map.get(nums[i]) != null){
-                ans[0] = map.get(nums[i]);
-                ans[1] = i;
-                return ans;
+
+            if(m.containsKey(nums[i])){
+                op[0] = m.get(nums[i]);
+                op[1] = i;
+                return op;
             }else{
-                map.put(target - nums[i], i);
+                m.put(target - nums[i], i);
             }
-            
         }
-        
-        return new int[2];
+
+        return op;
 
     }
     
+
+    public int[] twoSum2(int[] nums, int target) {
+
+        int [] op = new int [2];
+
+        for(int i=0;i<nums.length;i++){
+            for(int j = i + 1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    op[0] = i;
+                    op[1] = j;
+                    return op;
+                }
+            }
+        }
+
+        return op;
+    }
 }
