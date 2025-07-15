fde948bb77f3e9e8b4a78cf16afa789028d1af69
Abhishek Pawaskar <abhi.pro8898@gmail.com>
2025-07-16 00:26:11 +0530

    updated code

--- DSA-Practice/Problems/Java/Array/SearchInRotatedSortedArray.java
+++ DSA-Practice/Problems/Java/Array/SearchInRotatedSortedArray.java
@@ -30,26 +30,35 @@ public class SearchInRotatedSortedArray {
     }
 
     public int search2(int[] nums, int target) {
+
         int start = 0;
         int end = nums.length - 1;
-        int mid = 0;
+
         while (start <= end) {
-            mid = (start + end) / 2;
+
+            int mid = (start + end) / 2;
+
             if (nums[mid] == target)
                 return mid;
-            if (nums[mid] <= nums[end]) {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            } else {
-                if (target > nums[mid] || target <= nums[end]) {
+
+            // if we in left sorted array
+            if (nums[start] <= nums[mid]) {
+                if (target < nums[start] || target > nums[mid]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
                 }
             }
+            // in left sorted array
+            else {
+                if (target > nums[end] || target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+
+            }
+
         }
 
         return -1;
